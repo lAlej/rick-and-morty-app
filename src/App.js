b946258b167f217b0334553cc1cd1ceb315cad30
Lape <l.a.esteves99@gmail.com>
2023-03-13 08:32:57 -0400

    Prueba 2

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import Spec from './components/Spec';
 
 function App() {
   return (
-    <Routes>
-          <h1>Funciona</h1>
-          <Route path='/' element={ <ApiCall/> }/>
-          <Route path='/specs/*' element={ <Spec/> }/>
-          <Route path='/location/*' element={ <Location/> }/>
-        </Routes>
+    // <Routes>
+    //       <h1>Funciona</h1>
+    //       <Route path='/' element={ <ApiCall/> }/>
+    //       <Route path='/specs/*' element={ <Spec/> }/>
+    //       <Route path='/location/*' element={ <Location/> }/>
+    // </Routes>
+    <h1>Funcionando</h1>
 
   );
 }
